2d45652f568a7827dee4291b17f5201245f4cc33
Kenny Davis <kjdavis2423@gmail.com>
2018-03-21 20:51:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,9 @@ function sayHiToGrandma(string){
   if(string.toLowerCase() === string){
     return "I can't hear you!"
   }
-}
-function sayHiToGrandma(string){
   if(string.toUpperCase() === string){
     return "YES INDEED!"
   }
-}
-function sayHiToGrandma(string){
   if(string == "I love you, Grandma."){
     return "I love you, too."
   }
