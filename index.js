a6ed776e7a1d833d2b8e7d56d42df6a11fafe9c5
Kenny Davis <kjdavis2423@gmail.com>
2018-03-21 20:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   }
 }
+function sayHiToGrandma(string){
+  if(string.toUpperCase() === string){
+    return "YES INDEED!"
+  }
+}
+function sayHiToGrandma(string){
+  if(string == "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
